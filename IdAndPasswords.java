4c878ea5d38cecc58883e0663dacbb0a00e1128d
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-04-02 21:55:00 +0200

    Update IdAndPasswords.java

--- IdAndPasswords.java
+++ IdAndPasswords.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 public class IdAndPasswords{
     HashMap<String,String> logininfo = new HashMap<>();
     IdAndPasswords(){
-        logininfo.put("Artur","koty123");
-        logininfo.put("Kacper","PASSWORD");
+        logininfo.put("Artur","kot123");
+        logininfo.put("Ola","PASSWORD");
         logininfo.put("Ula","abc123");
+        logininfo.put("admin","root");
     }
    protected HashMap getLoginInfo(){
         return logininfo;
     }
-
 }
