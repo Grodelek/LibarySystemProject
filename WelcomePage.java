25cf83e31d5884bb666470bfd7faf339816cd3c8
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-04-02 21:11:11 +0200

    Created Menu Class

--- WelcomePage.java
+++ WelcomePage.java
@@ -1,2 +1,20 @@
-public class WelcomePage {
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuPage {
+    JFrame frame = new JFrame();
+    MenuPage(String name){
+    JLabel welcomeLabel = new JLabel("Hello "+name+"!");
+        welcomeLabel.setBounds(0,0,100,35);
+        welcomeLabel.setFont(new Font(null, Font.PLAIN,20));
+        frame.add(welcomeLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(460,430);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (screenSize.width - frame.getWidth()) /2;
+        int y = (screenSize.height - frame.getHeight())/2;
+        frame.setLocation(x, y);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
