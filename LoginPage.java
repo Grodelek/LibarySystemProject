4c026ff28c6f1e4a052b71b3c2bbc3127492a1f1
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-04-03 19:52:57 +0200

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -52,6 +52,7 @@ public class LoginPage extends JPanel implements ActionListener {
         frame.setVisible(true);
 
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
     if(e.getSource() == resetButton){
